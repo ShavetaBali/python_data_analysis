f6edebde3d22b16a7da7f180bb80100d528adbc7
sb0704 <shaveta.bali@paramount.com>
2025-06-15 14:11:48 -0500

    Updatemaincode

--- main.py
+++ main.py
@@ -15,10 +15,31 @@ except FileNotFoundError:
 
 # 2 Perform some basic analysis
 print("\n Basic statistics:")
-print(df.describe())
+#print(df.describe())
+
+# Calculate the average scores for each subject
+df['average_score'] = df[['Math', 'Science', 'English', 'Art']].mean(axis=1)
+print(f"\n--Average scores per subject---- = \n {df['average_score']}")
+
+df
+
 
 # 3 create some visualization
+# Create a figure with two subplots side by side
+fig, axes = plt.subplots(1, 2, figsize=(18, 7))
+
+# Plot 1 :Bar chart for Average Scores of students
+axes[0].bar(df['Student'], df['average_score'], color ='blue')
+axes[0].set_xlabel('Student' , fontsize=20)
+axes[0].set_ylabel('Average Score', fontsize=20)
+axes[0].set_title('Average Score of Students', fontsize=18)
+axes[0].grid(axis='y' ,linestyle='--',alpha=0.7)
+axes[0].tick_params(axis='x',rotation=45) # rotate x-axis labels for readability
 
 
-
-
+# # Plot 2 Bar chart for Average Scores per subject
+#
+#
+plt.tight_layout()
+plt.show()
+print("\n Analysis compleet and plot is displayed" )
